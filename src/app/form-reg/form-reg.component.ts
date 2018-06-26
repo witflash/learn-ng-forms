9884a7500ed7f3d0b4654e2288b28ce4440017fb
witflash <witflash@gmail.com>
2018-06-27 02:19:59 +0300

    add phone number function

--- src/app/form-reg/form-reg.component.ts
+++ src/app/form-reg/form-reg.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-form-reg',
@@ -15,19 +15,37 @@ export class FormRegComponent {
       'password': new FormControl('', Validators.required),
       'gender': new FormControl('', Validators.required),
       'subscribe': new FormControl('false'),
-      'email': new FormControl({value: '', disabled: true}, Validators.required),
+      'phones': new FormArray([
+        new FormControl('', [Validators.required, Validators.pattern('[0-9]+')]),
+      ]),
+      'email': new FormControl({value: '', disabled: true}, [Validators.required, Validators.email]),
+      'confirm': new FormControl('', [Validators.required, this.confirmValidator])
     })
-   }
+  }
 
-   ngOnInit() {
-     this.regForm.controls['subscribe'].valueChanges.subscribe(value => {
-       console.log(value);
-       this.regForm.get('email')[value == 'true' ? 'enable' : 'disable']();
-     });
-   }
+  ngOnInit() {
+    this.regForm.controls['subscribe'].valueChanges.subscribe(value => {
+      this.regForm.get('email')[value === 'true' ? 'enable' : 'disable']();
+    });
+  }
 
   submit() {
     console.log(this.regForm.controls['subscribe']);
+  }
+
+  confirmValidator(control: FormControl): {[s:string]:boolean} {
+    if(control.value === false) {
+      return {'confirm': true};
+    }
+    return null;
+  }
+
+  addPhone() {
+    (<FormArray>this.regForm.controls['phones']).push(new FormControl('', Validators.required));
+  }
+
+  removePhone() {
+    (<FormArray>this.regForm.controls['phones']).removeAt(-1);
   }
 
 }
